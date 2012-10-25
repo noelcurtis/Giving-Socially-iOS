67bce5ae651a333acd5ffd21025a3a979e0d27ef
Scott Penrose <penrose@gmail.com>
2012-10-24 22:14:30 -0400

    Remove a shit load of bullshit

--- Code/Models/GSActivity.h
+++
@@ -1,5 +0,0 @@
-#import "_GSActivity.h"
-
-@interface GSActivity : _GSActivity {}
-// Custom logic goes here.
-@end
--- Code/Models/GSActivity.m
+++
@@ -1,7 +0,0 @@
-#import "GSActivity.h"
-
-@implementation GSActivity
-
-// Custom logic goes here.
-
-@end
